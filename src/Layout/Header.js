d3e9071b2fc56eff90ecbb5acffc8c2043377312
Nicholas Hooley <nicholashooley27@yahoo.co.uk>
2022-06-21 18:58:56 +0200

    create header button component

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -1,13 +1,14 @@
 import { Fragment } from 'react';
 import mealImage from '../../src/assets/meals.jpeg';
 import classes from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
 
 const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>Meals4u</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={classes['main-image']}>
         <img src={mealImage} alt="food init" />
