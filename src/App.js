6e23ed0f7dae031da860e1f4e83e96539d198eac
Nicholas Hooley <nicholashooley27@yahoo.co.uk>
2022-06-22 08:41:36 +0200

    created meals list and summary components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { Fragment } from 'react';
 import Header from './Layout/Header';
+import Meals from './Meals/Meals';
+
 function App() {
   return (
     <Fragment>
       <Header />
+      <Meals />
     </Fragment>
   );
 }
