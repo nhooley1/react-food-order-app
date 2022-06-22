ea07591ed89646d5c713ae5d742f9004240d1743
Nicholas Hooley <nicholashooley27@yahoo.co.uk>
2022-06-22 13:29:38 +0200

    added close functionality to backdrop

--- src/Cart/Cart.js
+++ src/Cart/Cart.js
@@ -11,7 +11,7 @@ const Cart = (props) => {
   );
 
   return (
-    <Modal>
+    <Modal onClick={props.onCloseModel}>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
